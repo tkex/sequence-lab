85244f21e05bef6ca6481fcf6c9fe3f13e8c3f6f
tkex <>
2023-11-20 10:51:48 +0100

    feat: add pygame inits and window props

---
+++ sequence-lab.py
@@ -0,0 +1,11 @@
+
+import pygame
+import sys
+
+# Initialize
+pygame.init()
+
+# Define pygame window props
+width, heigth = 300, 300
+window = pygame.display.set_mode((width, heigth))
+pygame.display.set_caption("Sequence lab!")
