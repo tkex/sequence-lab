2690ecf36e008c87f9057d72d9f5cc2164602c95
tkex <>
2023-11-20 11:00:11 +0100

    feat: add sequence UI grid

--- sequence-lab.py
+++ sequence-lab.py
@@ -9,3 +9,35 @@ pygame.init()
 width, heigth = 300, 300
 window = pygame.display.set_mode((width, heigth))
 pygame.display.set_caption("Sequence lab!")
+
+# Define colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+def grid():
+    # Size for each memory field
+    blockSizeForField = 100
+    # Go through the grid in horizontal (x) and vertical (y) axis
+    for x in range(0, width, blockSizeForField):
+        for y in range(0, heigth, blockSizeForField):
+            # Create rectangle at position x, y (with size blockSize * blockSize)
+            rectangle = pygame.Rect(x, y, blockSizeForField, blockSizeForField)
+            # Create rectangle at window
+            # width = 1: only the border is drawn
+            pygame.draw.rect(window, WHITE, rectangle, 1)
+
+# Loop
+while True:
+    # Close event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Background colour
+    window.fill(BLACK)
+
+    # Draw grid
+    grid()
+
+    pygame.display.update()
